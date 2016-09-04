c23211ad9aae9c8c884e8d5877ad5c85f3fd3f23
subha-py <subha4exam@gmail.com>
2016-09-04 13:07:48 +0530

    Chapter one complete

--- chapter_1/scrapetest.py
+++ chapter_1/scrapetest.py
@@ -1,4 +1,21 @@
-from urllib.request import urlopen
+from urllib.request import urlopen,HTTPError
+from bs4 import BeautifulSoup
 
-html=urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+def getTitle(url):
+    try:
+        html=urlopen(url)
+    except HTTPError as e:
+        print(e)
+    try:
+        bs_obj=BeautifulSoup(html.read(),'html.parser')
+        title=bs_obj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title=getTitle("http://www.pythonscraping.com/pages/page1.html")
+
+if title==None:
+    print("Title could not be found")
+else:
+    print(title)
